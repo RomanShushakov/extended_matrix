8735b6ab0b6d8134008404ba3d5675531f9ef2cc
roman shushakov <roman.a.shushakov1@gmail.com>
2022-11-09 11:58:06 +0500

    lup_decomp method has been added to SquareMatrixTrait

--- src/matrix/traits/square_matrix_trait.rs
+++ src/matrix/traits/square_matrix_trait.rs
@@ -94,7 +94,6 @@ fn substitute_gep<V, SMT, VT1, VT2>(a: &SMT, n: usize, b: &VT1, x: &mut VT2)
           VT1: VectorTrait + BasicOperationsTrait<Value = V>,
           VT2: VectorTrait + BasicOperationsTrait<Value = V>,
 {
-    let f = |data: &str| println!("{}", data);
     *x.get_mut_element_value(&Position(n - 1, 0)).expect("Element is absent") = 
         *b.get_element_value(&Position(n - 1, 0)).expect("Element is absent") / 
         *a.get_element_value(&Position(n - 1, n - 1)).expect("Element is absent");
@@ -109,6 +108,115 @@ fn substitute_gep<V, SMT, VT1, VT2>(a: &SMT, n: usize, b: &VT1, x: &mut VT2)
         *x.get_mut_element_value(&Position(i, 0)).expect("Element is absent") = 
             (*b.get_element_value(&Position(i, 0)).expect("Element is absent") - sum) / 
             *a.get_element_value(&Position(i, i)).expect("Element is absent");
+    }
+}
+
+
+fn pivot_lup<V, SMT, VT>(a: &mut SMT, o: &mut [usize], s: &[<SMT as BasicOperationsTrait>::Value], n: usize, k: usize)
+where V: FloatTrait,
+      SMT: SquareMatrixTrait,
+      VT: VectorTrait,
+      <SMT as BasicOperationsTrait>::Value: FloatTrait<Output = V>,
+      <VT as BasicOperationsTrait>::Value: FloatTrait<Output = V>,
+{
+    let mut p = k;
+    let mut big = (*a.get_element_value(&Position(o[k], k))
+        .expect("Element is absent") / s[o[k]]).my_abs();
+    for ii in k + 1..n
+    {
+        let dummy = (*a.get_element_value(&Position(o[ii], k))
+            .expect("Element is absent") / s[o[ii]]).my_abs();
+        if dummy > big
+        {
+            big = dummy;
+            p = ii;
+        }
+    }
+    let dummy = o[p];
+    o[p] = o[k];
+    o[k] = dummy;
+}
+
+
+fn decompose_lup<V, SMT, VT>(a: &mut SMT, n: usize, rel_tol: <SMT as BasicOperationsTrait>::Value,
+    o: &mut [usize], s: &mut [<SMT as BasicOperationsTrait>::Value]) -> Result<(), String>
+    where V: FloatTrait,
+          SMT: SquareMatrixTrait + BasicOperationsTrait<Value = V>,
+          VT: VectorTrait + BasicOperationsTrait<Value = V>,
+          <SMT as BasicOperationsTrait>::Value: FloatTrait<Output = V>,
+          <VT as BasicOperationsTrait>::Value: FloatTrait<Output = V>,
+{
+    for i in 0..n
+    {
+        o[i] = i;
+        s[i] = (*a.get_element_value(&Position(i, 0)).expect("Element is absent")).my_abs();
+        for j in 1..n
+        {
+            if (*a.get_element_value(&Position(i, j)).expect("Element is absent")).my_abs() > s[i]
+            {
+                s[i] = (*a.get_element_value(&Position(i, j)).expect("Element is absent")).my_abs();
+            }
+        }
+    };
+    for k in 0..n - 1
+    {
+        pivot_lup::<V, SMT, VT>(a, o, s, n, k);
+        if (*a.get_element_value(&Position(o[k], k)).expect("Element is absent") / s[o[k]]).my_abs() < rel_tol
+        {
+            return Err("Ill conditioned system".to_string());
+        }
+        for i in k + 1..n
+        {
+            let factor = *a.get_element_value(&Position(o[i], k)).expect("Element is absent") /
+                *a.get_element_value(&Position(o[k], k)).expect("Element is absent");
+            *a.get_mut_element_value(&Position(o[i], k)).expect("Element is absent") = factor;
+            for j in k + 1..n
+            {
+                *a.get_mut_element_value(&Position(o[i], j)).expect("Element is absent") = 
+                    *a.get_element_value(&Position(o[i], j)).expect("Element is absent") - factor * 
+                    *a.get_element_value(&Position(o[k], j)).expect("Element is absent");
+            }
+        }
+    }
+    if (*a.get_element_value(&Position(o[n - 1], n - 1)).expect("Element is absent") / s[o[n - 1]])
+        .my_abs() < rel_tol
+    {
+        return Err("Ill conditioned system".to_string());
+    }
+    Ok(())
+}
+
+
+fn substitute_lup<V, SMT, VT1, VT2>(a: &SMT, o: &[usize], n: usize, b: &mut VT1, x: &mut VT2)
+    where V: FloatTrait<Output = V>,
+          SMT: SquareMatrixTrait + BasicOperationsTrait<Value = V>,
+          VT1: VectorTrait + BasicOperationsTrait<Value = V>,
+          VT2: VectorTrait + BasicOperationsTrait<Value = V>,
+{
+    for i in 1..n
+    {
+        let mut sum = *b.get_element_value(&Position(o[i], 0)).expect("Element is absent");
+        for j in 0..i
+        {
+            sum -= *a.get_element_value(&Position(o[i], j)).expect("Element is absent") * 
+                *b.get_element_value(&Position(o[j], 0)).expect("Element is absent");
+        }
+        *b.get_mut_element_value(&Position(o[i], 0)).expect("Element is absent") = sum;
+    }
+    *x.get_mut_element_value(&Position(n - 1, 0)).expect("Element is absent") = 
+        *b.get_element_value(&Position(o[n - 1], 0)).expect("Element is absent") / 
+        *a.get_element_value(&Position(o[n - 1], n - 1)).expect("Element is absent");
+    for i in (0..n - 1).rev()
+    {
+        let mut sum = V::from(0f32);
+        for j in i + 1..n
+        {
+            sum += *a.get_element_value(&Position(o[i], j)).expect("Element is absent") *
+                *x.get_element_value(&Position(j, 0)).expect("Element is absent");
+        }
+        *x.get_mut_element_value(&Position(i, 0)).expect("Element is absent") = 
+            (*b.get_element_value(&Position(o[i], 0)).expect("Element is absent") - sum) / 
+            *a.get_element_value(&Position(o[i], i)).expect("Element is absent");
     }
 }
 
@@ -195,6 +303,39 @@ pub trait SquareMatrixTrait: BasicOperationsTrait
         };
         let _ = eliminate_gep(&mut a, &mut s, n, &mut b, rel_tol)?;
         substitute_gep(&a, n, &b, x);
+        Ok(())
+    }
+
+
+    fn lup_decomp<VT1, VT2>(&self, b: &VT1, x: &mut VT2, rel_tol: <Self as BasicOperationsTrait>::Value) -> Result<(), String>
+        where VT1:  VectorTrait + BasicOperationsTrait<Value = <Self as BasicOperationsTrait>::Value> + Clone,
+              VT2:  VectorTrait + BasicOperationsTrait<Value = <Self as BasicOperationsTrait>::Value> + Clone,
+              Self: Clone,
+              <Self as BasicOperationsTrait>::Value: FloatTrait<Output = <Self as BasicOperationsTrait>::Value>,
+    {
+        let mut a = self.clone();
+        let mut b = b.clone();
+        b.vector_shape_conformity_check()?;
+        x.vector_shape_conformity_check()?;
+        if b.get_shape().0 == 1
+        {
+            b = b.transpose();
+        }
+        if x.get_shape().0 == 1
+        {
+            *x = x.transpose();
+        }
+        a.shape_conformity_check(&b, Operation::Multiplication)?;
+        b.shape_conformity_check(x, Operation::Addition)?;
+
+        let n = a.get_shape().0;
+        let mut o = vec![0usize; n];
+        let mut s = 
+            vec![<<Self as BasicOperationsTrait>::Value>::from(0f32); n];
+
+        decompose_lup::<<Self as BasicOperationsTrait>::Value, Self, VT1>(&mut a, n, rel_tol, &mut o, &mut s)?;
+        substitute_lup(&a, &o, n, &mut b, x);
+        
         Ok(())
     }
 }
--- src/tests/matrix/traits/test_square_matrix_trait.rs
+++ src/tests/matrix/traits/test_square_matrix_trait.rs
@@ -96,3 +96,52 @@ fn test_gauss_gep() -> Result<(), String>
 
     Ok(())
 }
+
+
+#[test]
+fn test_lup_decomp() -> Result<(), String>
+{
+    let a_1 = SquareMatrix::create(3, &[
+        3.0, -0.1, -0.2,
+        0.1, 7.0, -0.3,
+        0.3, -0.2, 10.0,
+    ]);
+    let b_1 = Vector::create(&[7.85, -19.3, 71.4]);
+    let mut x_1 = Vector3::create(&[0.0; 3]);
+
+    let a_2 = SquareMatrix::create(4, &[
+        5.0, -4.0, 1.0, 0.0,
+        -4.0, 6.0, -4.0, 1.0,
+        1.0, -4.0, 6.0, -4.0,
+        0.0, 1.0, -4.0, 5.0,
+    ]);
+    let b_2 = Vector::create(&[0.0, 1.0, 0.0, 0.0]);
+    let mut x_2 = Vector::create(&[0.0; 4]);
+
+    let a_3 = SquareMatrix::create(5, &[
+        2.0f32, -2.0, 0.0, 0.0, -1.0,
+        -2.0, 3.0, -2.0, 0.0, 0.0,
+        0.0, -2.0, 5.0, -3.0, 0.0,
+        0.0, 0.0, -3.0, 10.0, 4.0,
+        -1.0, 0.0, 0.0, 4.0, 10.0,
+    ]);
+
+    let mut b_3 = Vector::create(&[0.0, 1.0, 0.0, 0.0, 0.0]);
+    b_3 = b_3.transpose();
+    let mut x_3 = Vector::create(&[0.0; 5]);
+
+    let expected_x_1 = Vector3::create(&[3.0, -2.5, 7.000000000000002]);
+    let expected_x_2 = Vector::create(&[1.6000000000000028, 2.6000000000000045, 
+        2.400000000000004, 1.4000000000000024]);
+    let expected_x_3 = Vector::create(&[635.995, 618.9951, 291.99768, 73.99941, 33.999725]);
+
+    a_1.lup_decomp(&b_1, &mut x_1, 1e-6)?;
+    a_2.lup_decomp(&b_2, &mut x_2, 1e-6)?;
+    a_3.lup_decomp(&b_3, &mut x_3, 1e-6)?;
+
+    assert_eq!(x_1, expected_x_1);
+    assert_eq!(x_2, expected_x_2);
+    assert_eq!(x_3, expected_x_3);
+
+    Ok(())
+}
